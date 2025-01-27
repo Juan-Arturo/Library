56506fa343eb7723ca2fe7bdc00e3517d934c275
Juan-Arturo <jagpjagp002@gmail.com>
2025-01-27 09:25:58 -0600

    modificaciones e instalaciones

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -7,41 +7,91 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
-
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 #[Route('/library', name: 'library')]
 
 final class LibraryController extends AbstractController
 {
 
-    #[Route('/list', name: 'library_list')]
-    public function list(Request $request, LoggerInterface $logger): JsonResponse
+    #[Route('/books/list', name: 'library_list')]
+    public function list(Request $request, BookRepository $BookRepository): JsonResponse
     {
-        $title = $request->get('title', 'titulo de respaldo');
-        $logger->info('List action called 2 ');
+       // $title = $request->get('title', 'titulo de respaldo');
+       // $logger->info('List action called 2 ');
+       
+       
+       //Hacemos un array de books usando el findAll de booksRepository
+        $books = $BookRepository->findAll();
+        $booksArray = [];
+        foreach ($books as $book) {
+            $booksArray[] = [
+                'id' => $book->getId(),
+                'title' => $book->getTitle(),
+                'image' => $book->getImage(),
+            ];
+        }
+
+        //Preparamos la respuesta JSON con el array de books
         $response = new JsonResponse();
+        $response->setData([
+            'succes' => true,
+            'data' => $booksArray,
+                
+        ]);
+
+        return $response;
+    }
+
+    #[Route('/books/create', name: 'create_book')]
+    public function createBook(Request $request, EntityManagerInterface $em): JsonResponse
+    {
+        //lo asocio a la entidad book
+        $book = new Book();
+
+        //Preparamos la respuesta JSON
+        $response = new JsonResponse();
+
+
+        //buscamos el title de la entidad book
+        $title = $request->get('title',null);
+
+
+        //comparativo de null
+        if($title == null){
+            $response->setData([
+                'succes' => false,
+                'error' => [
+                    'message' => 'El campo title no puede ser null',
+                    'code' => 400
+                ]
+            ]);
+            return $response;
+        }
+
+        //uso el setter de la entidad book
+        $book->setTitle($title);
+
+        //Comunico a EntityManagerInterface de la existencia de la entidad book
+        $em->persist($book);
+
+        //Guardo los cambios en la base de datos en este caso los cambios son la existencia de la entidad book
+        $em->flush();
 
         $response->setData([
             'succes' => true,
             'data' => [
                 [
-                    'id' => 1,
-                    'title' => "El esclavo",
-                ],
-                [
-                    'id' => 2,
-                    'title' => "Game of Thrones",
-                ],
-                [
-                    'id' => 3,
-                    'title' => $title,
+                    'id' => $book->getId(),
+                    'title' => $book->getTitle(),
                 ]
-                
             ]
         ]);
-        return $response;
-    }
 
+        return $response; 
+    }
 
 }
 
